9a7a007e3aef3823cf4f95e453d574a80f268aa7
Navaneeth Bysani <61292292+Navaneeth-Bysani@users.noreply.github.com>
2022-02-08 21:12:41 +0530

    few changes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const app = require('./app');
 
-app.listen(4000, () => {
+app.listen(3000, () => {
     console.log('Running on port 4000...')
 })
-
-app.use('/', (req,res,next)=> {
-    res.send('Hi');
+app.set('views', 'html')
+app.use('/', async (req,res,next)=> {
+    try {
+        res.send('Hi');
+    } catch(err) {
+        console.log(err);
+    }
 })
